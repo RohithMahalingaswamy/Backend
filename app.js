c78d4d2c46d9cf42df4eff6eed0285178e3c65f3
Rohithmahalingaswamy <rohith.rm94@gmail.com>
2021-04-14 12:08:17 +0530

    catch error code  added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const { response } = require('express');
 app.use(bodyParser.urlencoded({ extended: false }));          
  app.use(bodyParser.json())
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -16,36 +17,25 @@ app.get("/UkNews", function (req, res) {
     console.log("connected to UkNews")
     return axios.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=592f6b83b4ab4ec586baf7f9436206c3")
         .then(response => {
-            console.log("responseeeee",response.data)
             res.status(202).send(response.data);
-            //res.json({"status":200,"data":response.data});
-            // res.send(response.data) // <= send data to the client
-            //  res.send("Hello World!");
 
         })
         .catch(err => {
-            console.log("errorhandler", err)
-            res.send({ err }) // <= send error
+            res.status(500).send(err);
         })
 
 })
-
-
 
 app.listen(8080, function () {
     console.log("server restared")
 })
 app.post("/filterKeyword", (req, res) => {
     var keyword=req.body.filter;
-    console.log("welcome",keyword)
-
     axios.get(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=592f6b83b4ab4ec586baf7f9436206c3`)
     .then(response=>{
         res.status(202).send(response.data);
-
     })
     .catch(err => {
-        console.log("errorpost", err)
-        res.send({ err }) // <= send error
+        res.status(500).send(err);
     })
 })
