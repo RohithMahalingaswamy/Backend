4267d79eedba4c75b1e06bb09f9c3fda707d7c0d
Rohithmahalingaswamy <rohith.rm94@gmail.com>
2021-04-14 16:22:16 +0530

    extra code removed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-const { response } = require('express');
 app.use(bodyParser.urlencoded({ extended: false }));          
  app.use(bodyParser.json())
 
